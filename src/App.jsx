0e7a9bb41d76c816be8330f3594480d46e8a5696
OjagaO <ahiroto1234@gmail.com>
2024-01-04 08:44:53 +0900

    [add]help-modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Viewer from "./components/Viewer";
 import "./App.css";
+import Help from "./components/Help";
 
 const App = () => {
     return (
         <div className="App">
             <Viewer />
+            <Help />
         </div>
     );
 };
